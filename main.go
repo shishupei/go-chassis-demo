4457eb9232ecbeb76472bca46d4e32fbd08c366f
shishupei <shishupei10@163.com>
2024-12-10 10:10:34 +0800

    init

--- main.go
+++ main.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chassis/go-chassis/v2"
+	"go-chassis-demo/schema"
 	"log"
-	"net/http"
 )
-
-// HelloWorldHandler 定义一个处理请求的函数
-func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	// 设置响应头
-	w.Header().Set("Content-Type", "application/json")
-	// 返回 HelloWorld 消息
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, `{"message": "Hello, ServiceComb with GoChassis!"}`)
-}
 
 func main() {
 	// 初始化 GoChassis
+	chassis.RegisterSchema("rest", &schema.Hello{})
 	err := chassis.Init()
 	if err != nil {
 		log.Fatalf("Failed to initialize GoChassis: %v", err)
 	}
-
-	// 创建一个 REST 服务，并注册路由
-	http.HandleFunc("/hello", HelloWorldHandler)
-
-	// 启动服务
-	port := ":8080"
-	log.Printf("Starting HelloWorld API on port %s", port)
-	err = http.ListenAndServe(port, nil)
-	if err != nil {
-		log.Fatalf("Error starting server: %v", err)
-	}
+	chassis.Run()
 }
---
+++ schema/hello.go
@@ -0,0 +1,117 @@
+package schema
+
+import (
+	"errors"
+	"net/http"
+
+	"fmt"
+	"math/rand"
+
+	rf "github.com/go-chassis/go-chassis/v2/server/restful"
+)
+
+var num = rand.Intn(100)
+
+// RestFulHello is a struct used for implementation of restfull hello program
+type RestFulHello struct {
+}
+
+// Sayhello is a method used to reply user with hello
+func (r *RestFulHello) Root(b *rf.Context) {
+	b.Write([]byte(fmt.Sprintf("x-forwarded-host %s", b.ReadRequest().Host)))
+}
+
+// Sayhello is a method used to reply user with hello
+func (r *RestFulHello) Sayhello(b *rf.Context) {
+	b.Write([]byte("hello"))
+}
+
+// URLPatterns helps to respond for corresponding API calls
+func (r *RestFulHello) URLPatterns() []rf.Route {
+	return []rf.Route{
+		{Method: http.MethodGet, Path: "/", ResourceFunc: r.Root,
+			Returns: []*rf.Returns{{Code: 200}}},
+
+		{Method: http.MethodGet, Path: "/sayhello/{userid}", ResourceFunc: r.Sayhello,
+			Returns: []*rf.Returns{{Code: 200}}},
+	}
+}
+
+// RestFulMessage is a struct used to implement restful message
+type RestFulMessage struct {
+}
+
+// Saymessage is used to reply user with his name
+func (r *RestFulMessage) Saymessage(b *rf.Context) {
+	id := b.ReadPathParameter("name")
+
+	b.Write([]byte("get name: " + id))
+}
+
+// Sayhi is a method used to reply request user with hello world text
+func (r *RestFulMessage) Sayhi(b *rf.Context) {
+	result := struct {
+		Name string
+	}{}
+	err := b.ReadEntity(&result)
+	if err != nil {
+		b.Write([]byte(err.Error() + ":hello world"))
+		return
+	}
+	b.Write([]byte(result.Name + ":hello world"))
+	return
+}
+
+// Sayerror is a method used to reply request user with error
+func (r *RestFulMessage) Sayerror(b *rf.Context) {
+	b.WriteError(http.StatusInternalServerError, errors.New("test hystric"))
+	return
+}
+
+// URLPatterns helps to respond for corresponding API calls
+func (r *RestFulMessage) URLPatterns() []rf.Route {
+	return []rf.Route{
+		{Method: http.MethodGet, Path: "/saymessage/{name}", ResourceFunc: r.Saymessage},
+		{Method: http.MethodPost, Path: "/sayhimessage", ResourceFunc: r.Sayhi},
+		{Method: http.MethodGet, Path: "/sayerror", ResourceFunc: r.Sayhi},
+	}
+}
+
+// Hello is a struct used for implementation of restfull hello program
+type Hello struct{}
+
+// Hello
+func (r *Hello) Hello(b *rf.Context) { b.Write([]byte("hi from hello")) }
+
+// URLPatterns helps to respond for corresponding API calls
+func (r *Hello) URLPatterns() []rf.Route {
+	return []rf.Route{
+		{Method: http.MethodGet, Path: "/hello", ResourceFunc: r.Hello},
+	}
+}
+
+// Legacy is a struct
+type Legacy struct{}
+
+// Do
+func (r *Legacy) Do(b *rf.Context) { b.Write([]byte("hello from legacy")) }
+
+// URLPatterns helps to respond for corresponding API calls
+func (r *Legacy) URLPatterns() []rf.Route {
+	return []rf.Route{
+		{Method: http.MethodGet, Path: "/legacy", ResourceFunc: r.Do},
+	}
+}
+
+// Legacy is a struct
+type Admin struct{}
+
+// Do
+func (r *Admin) Do(b *rf.Context) { b.Write([]byte("hello from admin")) }
+
+// URLPatterns helps to respond for corresponding API calls
+func (r *Admin) URLPatterns() []rf.Route {
+	return []rf.Route{
+		{Method: http.MethodGet, Path: "/admin", ResourceFunc: r.Do},
+	}
+}
